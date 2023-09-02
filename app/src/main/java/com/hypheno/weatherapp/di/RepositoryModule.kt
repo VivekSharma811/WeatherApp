c834af1c0afe4221984684db35afc5059c20ac22
Vivek <vivek.sharma@mpokket.com>
2023-09-02 10:46:33 +0530

    DI added

---
+++ app/src/main/java/com/hypheno/weatherapp/di/RepositoryModule.kt
@@ -0,0 +1,22 @@
+package com.hypheno.weatherapp.di
+
+import com.hypheno.weatherapp.data.repository.WeatherRepositoryImpl
+import com.hypheno.weatherapp.domain.repository.WeatherRepository
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import javax.inject.Singleton
+
+@ExperimentalCoroutinesApi
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+
+    @Binds
+    @Singleton
+    abstract fun bindWeatherRepository(
+        weatherRepositoryImpl: WeatherRepositoryImpl
+    ): WeatherRepository
+}
