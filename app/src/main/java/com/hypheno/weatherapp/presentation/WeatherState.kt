86fdb7a81605694f2c0d11463ac4dda67b34e750
Vivek <vivek.sharma@mpokket.com>
2023-09-02 10:54:08 +0530

    WeatherViewModel added

---
+++ app/src/main/java/com/hypheno/weatherapp/presentation/WeatherState.kt
@@ -0,0 +1,9 @@
+package com.hypheno.weatherapp.presentation
+
+import com.hypheno.weatherapp.domain.weather.WeatherInfo
+
+data class WeatherState(
+    val weatherInfo: WeatherInfo? = null,
+    val isLoading: Boolean = false,
+    val error: String? = null
+)
